45f610b65e77c434102adc30563208c22277bf43
Ali <aliabdelhakim33>
2021-09-08 20:51:51 +0200

    temp1

---
+++ js/script.js
@@ -0,0 +1,37 @@
+$(document).ready(function() {
+    $(".owl-carousel").owlCarousel();
+});
+$('.intro .owl-carousel').owlCarousel({
+    rtl: true,
+    loop: false,
+    margin: 15,
+    nav: true,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 2
+        },
+        1000: {
+            items: 3
+        }
+    }
+});
+$('.clients .owl-carousel').owlCarousel({
+    rtl: true,
+    loop: false,
+    margin: 15,
+    nav: true,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 2
+        },
+        1000: {
+            items: 4
+        }
+    }
+});
